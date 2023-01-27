e3295a6560f4f5b8f412f9550bbce7d5e7653c0c
JoshuaJonesTS <joshua@tailoredsoftware.us>
2023-01-27 15:04:08 -0800

    Create navigation

--- src/pages/Layout/Layout.page.jsx
+++ src/pages/Layout/Layout.page.jsx
@@ -1,11 +1,23 @@
 import React from 'react';
+import { Outlet, Link } from 'react-router-dom'
 
 const Layout = () => {
     return (
-        <div>
-            Layout
-        </div>
-    )
+        <>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/blogs">Blogs</Link>
+              </li>
+            </ul>
+          </nav>
+    
+          <Outlet />
+        </>
+      )
 }
 
 export default Layout;
