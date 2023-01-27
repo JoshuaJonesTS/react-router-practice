be7e8abeaee662cce931d350e4cea6e23fe34457
JoshuaJonesTS <joshua@tailoredsoftware.us>
2023-01-27 14:38:16 -0800

    Re-locate from App.js to index.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
+import logo from './logo.svg';
 import './App.css';
-import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Layout from './pages/Layout/Layout.page';
-import Home from './pages/Home/Home.page';
-import Blogs from './pages/Blogs/Blogs.page';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout/>}>
-        <Route index element={<Home/>} />
-        <Route path="blogs" element={<Blogs/>} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
