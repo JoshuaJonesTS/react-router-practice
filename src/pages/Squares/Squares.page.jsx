2a56f513e36dd76416eb46e209d2358bd7e16275
JoshuaJonesTS <joshua@tailoredsoftware.us>
2023-01-28 18:38:49 -0800

    Spacing

--- src/pages/Squares/Squares.page.jsx
+++ src/pages/Squares/Squares.page.jsx
@@ -3,9 +3,11 @@ import Square from '../../blocks/Square.block';
 
 const Squares = () => {
     let purpleSquares = [];
+
     for(let i = 1; i <= 20; i++) {
         purpleSquares.push(<Square color={'purple'} label={['purple ' + i, 'white']}/>);
     }
+    
     return (
         <div style={{
             display: 'flex',
