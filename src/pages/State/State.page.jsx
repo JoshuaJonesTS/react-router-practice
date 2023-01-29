29615d2eba53d2c125d210fc3d59c233d600cdcb
JoshuaJonesTS <joshua@tailoredsoftware.us>
2023-01-28 19:17:52 -0800

    Style button

--- src/pages/State/State.page.jsx
+++ src/pages/State/State.page.jsx
@@ -7,6 +7,13 @@ const State = () => {
         <div>
             <h1>This code is being typed on a laptop located in {state}</h1>
             <button
+            style={{
+                backgroundColor: 'rgb(34, 139, 34)',
+                borderRadius: '10px',
+                color: 'white',
+                fontWeight: '700',
+                padding: '10px'
+            }}
             type="button"
             onClick={
                 () => setState('Nevada')
