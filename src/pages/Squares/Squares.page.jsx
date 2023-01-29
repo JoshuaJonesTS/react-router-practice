2e7013656b8086cf66b9769516d5fee93a301e8e
JoshuaJonesTS <joshua@tailoredsoftware.us>
2023-01-28 18:33:54 -0800

    Make purple text white

--- src/pages/Squares/Squares.page.jsx
+++ src/pages/Squares/Squares.page.jsx
@@ -4,7 +4,7 @@ import Square from '../../blocks/Square.block';
 const Squares = () => {
     let purpleSquares = [];
     for(let i = 1; i <= 20; i++) {
-        purpleSquares.push(<Square color={'purple'} label={'purple ' + i}/>);
+        purpleSquares.push(<Square color={'purple'} label={['purple ' + i, 'white']}/>);
     }
     return (
         <div style={{
