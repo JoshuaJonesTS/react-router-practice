7f20b3ab114461a3e3def9f96e61525fd09a7a3e
JoshuaJonesTS <joshua@tailoredsoftware.us>
2023-01-28 18:26:00 -0800

    More squares

--- src/pages/Squares/Squares.page.jsx
+++ src/pages/Squares/Squares.page.jsx
@@ -3,7 +3,7 @@ import Square from '../../blocks/Square.block';
 
 const Squares = () => {
     let purpleSquares = [];
-    for(let i = 1; i <= 10; i++) {
+    for(let i = 1; i <= 20; i++) {
         purpleSquares.push(<Square color={'purple'} label={'purple ' + i}/>);
     }
     return (
