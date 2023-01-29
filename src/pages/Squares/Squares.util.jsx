424d95c746f5f458cd54728d25a5c2fe1741a44d
JoshuaJonesTS <joshua@tailoredsoftware.us>
2023-01-28 18:56:46 -0800

    Fix oversight

--- src/pages/Squares/Squares.util.jsx
+++ src/pages/Squares/Squares.util.jsx
@@ -5,7 +5,7 @@ const ArraySquares = (number, color) => {
     let squares = []; // hold square componets
 
     for(let i = 1; i <= number; i++) {
-        squares.push(<Square color={color} label={[color + ' ' + number, 'white']}/>);
+        squares.push(<Square color={color} label={[color + ' ' + i, 'white']}/>);
     }
 
     return squares;
