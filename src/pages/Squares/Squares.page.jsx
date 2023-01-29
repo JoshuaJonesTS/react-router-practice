6e2a2d97b7d0c6a8dca99fa9d87812d953558743
JoshuaJonesTS <joshua@tailoredsoftware.us>
2023-01-28 18:22:28 -0800

    Add purple squares

--- src/pages/Squares/Squares.page.jsx
+++ src/pages/Squares/Squares.page.jsx
@@ -2,6 +2,10 @@ import React from 'react';
 import Square from '../../blocks/Square.block';
 
 const Squares = () => {
+    let purpleSquares = [];
+    for(let i = 1; i <= 10; i++) {
+        purpleSquares.push(<Square color={'purple'} label={'purple ' + i}/>);
+    }
     return (
         <div style={{
             display: 'flex',
@@ -11,6 +15,7 @@ const Squares = () => {
             <Square color={'blue'} label={'blue 1'}/>
             <Square color={'green'} label={'green 1'}/>
             <Square color={'yellow'} label={'yellow 1'}/>
+            {purpleSquares}
         </div>
     );
 }
