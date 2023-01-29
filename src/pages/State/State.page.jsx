ab079d3a2ad9ed7939009da92c7ecbb218f1d1e7
JoshuaJonesTS <joshua@tailoredsoftware.us>
2023-01-28 19:13:17 -0800

    create page to practice useState

--- src/pages/State/State.page.jsx
+++ src/pages/State/State.page.jsx
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const State = () => {
+    const [state, setState] = useState('California');
+
     return (
         <div>
-            
+            <h1>This code is being typed on a laptop located in {state}</h1>
+            <button
+            type="button"
+            onClick={() => setState('Nevada')}>Nevada</button>
         </div>
     );
 }
