f9300b80963c2bd7f15c8d719eabdbf346007413
JoshuaJonesTS <joshua@tailoredsoftware.us>
2023-01-28 18:49:36 -0800

    Set up utility function

--- src/pages/Squares/Squares.util.jsx
+++ src/pages/Squares/Squares.util.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import Square from '../../blocks/Square.block';
 
-const ArraySquares = () => {
+const ArraySquares = ({number, color}) => {
+    let squares = []; // hold square componets
 
+    for(let i = 1; i <= number; i++) {
+        squares.push(<Square color={color} label={[color + ' ' + number, 'white']}/>);
+    }
 }
 
 export default ArraySquares;
