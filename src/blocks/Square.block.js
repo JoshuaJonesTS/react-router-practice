93512ff013e621a1bbb94c4deb83286ed9e0b0d1
JoshuaJonesTS <joshua@tailoredsoftware.us>
2023-01-28 18:28:47 -0800

    Enable label to hold an object

--- src/blocks/Square.block.js
+++ src/blocks/Square.block.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const Square = ({ color, label }) => {
+const Square = ({ color, label = []}) => {
     return (
         <div style={{
             backgroundColor: color,
