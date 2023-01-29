8e83000b94f07fa481fa0527f22def278dc58632
JoshuaJonesTS <joshua@tailoredsoftware.us>
2023-01-28 18:52:12 -0800

    Create object to hold component

--- src/pages/Squares/Squares.page.jsx
+++ src/pages/Squares/Squares.page.jsx
@@ -3,6 +3,8 @@ import Square from '../../blocks/Square.block';
 import ArraySquares from './Squares.util';
 
 const Squares = () => {
+    let arraySquares = ArraySquares(20, 'purple');
+    
     return (
         <div style={{
             display: 'flex',
@@ -13,6 +15,7 @@ const Squares = () => {
             <Square color={'blue'} label={['blue 1', 'white']}/>
             <Square color={'green'} label={['green 1', 'black']}/>
             <Square color={'yellow'} label={['yellow 1', 'black']}/>
+            {arraySquares}
         </div>
     );
 }
